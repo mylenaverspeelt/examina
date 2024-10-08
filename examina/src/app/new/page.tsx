94be44b211e5a8ab6231236524c3e33e09dc62e8
mylenaverspeelt <mylena49@hotmail.com>
2024-10-08 20:55:13 -0300

    style: improve css

--- examina/src/app/new/page.tsx
+++ examina/src/app/new/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css"
 export default function Home() {
     return (
         <div className={styles.new}>
-            <h1 className={styles.title}>Adicione um novo Exame:</h1>
+            <h1 className={styles.title}>Adicione um novo exame:</h1>
             <Upload />
         </div>
     );
--- examina/src/components/Navbar/Navbar.tsx
+++ examina/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image"
 import styles from "./Navbar.module.css"
 import Button from "../Button/Button"
 import Link from "next/link"
@@ -7,17 +6,11 @@ export default function Navbar() {
     return <nav className={styles.nav}>
         <div className={styles.logoDiv}>
         <Link href="/" passHref>
-                    <Image 
-                        src="/examina-logo.png" 
-                        alt="logo" 
-                        className={styles.logo} 
-                        width={150} 
-                        height={35} 
-                    />
+        <h1 className={styles.title}>examina</h1>
                 </Link>
         </div>
         <div className={styles.loginDiv}>
-            <Button label="Login / Register" />
+            <Button label="Entrar   / Registrar" />
         </div>
     </nav>
 }
