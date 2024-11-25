b58c1f39270d0904b1ce4346a79ee16d8da36da0
mylenaverspeelt <mylena49@hotmail.com>
2024-11-25 20:51:44 -0300

    refactor: menu button name

--- examina/src/app/page.tsx
+++ examina/src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className={styles.buttonDiv}>
         <Button label="Adicionar um novo exame" href="/new" icon={faCirclePlus} variant="menuButton" />
         <Button label="Ver exames armazenados" icon={faFolderOpen} href="/uploads" variant="menuButton" />
-        <Button label="Gerar Gráficos e Relatórios" icon={faChartLine} href="/analytics" variant="menuButton" />
+        <Button label="Visualizar Distribuição" icon={faChartLine} href="/analytics" variant="menuButton" />
       </div>
     </section>
   );
