7e9fb179d53d0dc3e0dac912834a5d1d655757f4
mylenaverspeelt <mylena49@hotmail.com>
2025-01-02 11:18:17 -0300

    feat: add prisma client

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,15 @@
-// import '@testing-library/jest-dom'; 
+jest.mock('@prisma/client', () => ({
+    PrismaClient: jest.fn().mockImplementation(() => ({
+      patient: {
+        findMany: jest.fn(),
+        findUnique: jest.fn(),
+      },
+      glucose: {
+        findMany: jest.fn(),
+      },
+      storage: {
+        findMany: jest.fn(),
+      },
+    })),
+  }));
+  
